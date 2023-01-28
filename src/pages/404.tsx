f1ba74350e76ea28ebfbaa896d90670d44852fbd
George Paresishvili <g.paresishvili@outlook.com>
2023-01-28 17:24:23 +0400

    removed package.lock - using yarn added error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const GP404: NextPage = () => {
+    return <main>
+        <h1>404</h1>
+    </main>
+}
+
+export default GP404
---
+++ src/pages/500.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from "next";
+
+const GP500: NextPage = () => {
+    return <main>
+        <h1>500</h1>
+    </main>
+}
+
+export default GP500
